014661c5978415aa6c38568b9f2ece6932ddd9c1
Ahmet Safa Mutaf <ahmetsafamutaf@gmail.com>
2025-12-16 21:41:57 +0300

    add initial domain entities

---
+++ src/AccountingApp/AccountingApp.Core/Entities/Account.cs
@@ -0,0 +1,23 @@
+﻿using AccountingApp.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingApp.Core.Entities
+{
+    public class Account : BaseEntity
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public AccountType Type { get; set; }
+        public int? ParentAccountId { get; set; }
+
+
+        public virtual Account ParentAccount { get; set; }
+        public virtual ICollection<Account> SubAccounts { get; set; }
+        public virtual ICollection<TransactionLine> TransactionLines { get; set; }
+    }
+}
---
+++ src/AccountingApp/AccountingApp.Core/Entities/Customer.cs
@@ -0,0 +1,25 @@
+﻿using AccountingApp.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace AccountingApp.Core.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string TaxNumber { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public CustomerType Type { get; set; }
+
+        public virtual ICollection<Transaction> Transactions { get; set; }
+        public virtual ICollection<Invoice> Invoices { get; set; }
+
+    }
+}
