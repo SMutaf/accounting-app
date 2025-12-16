45319cc4d0809e37f5a9290e97ca22daf479d67a
Ahmet Safa Mutaf <ahmetsafamutaf@gmail.com>
2025-12-16 21:50:48 +0300

    add invoice and invoiceItem entities

---
+++ src/AccountingApp/AccountingApp.Core/Entities/Invoice.cs
@@ -0,0 +1,27 @@
+﻿using AccountingApp.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace AccountingApp.Core.Entities
+{
+    public class Invoice : BaseEntity
+    {
+        public string InvoiceNumber { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public int CustomerId { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal TaxAmount { get; set; }
+        public decimal TotalAmount { get; set; }
+        public InvoiceType Type { get; set; }
+        public InvoiceStatus Status { get; set; }
+        public int? TransactionId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Transaction Transaction { get; set; }
+        public virtual ICollection<InvoiceItem> Items { get; set; }
+    }
+}
---
+++ src/AccountingApp/AccountingApp.Core/Entities/InvoiceItem.cs
@@ -0,0 +1,22 @@
+﻿using AccountingApp.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingApp.Core.Entities
+{
+    public class InvoiceItem : BaseEntity
+    {
+        public int InvoiceId { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TaxRate { get; set; }
+        public decimal TaxAmount { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public virtual Invoice Invoice { get; set; }
+    }
+}
