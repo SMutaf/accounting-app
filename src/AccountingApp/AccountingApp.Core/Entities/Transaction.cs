104515f62179c8048b87196ddb6e15cad96dbf88
Ahmet Safa Mutaf <ahmetsafamutaf@gmail.com>
2025-12-17 15:01:51 +0300

    add transaction and transactionLine entities

---
+++ src/AccountingApp/AccountingApp.Core/Entities/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using AccountingApp.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingApp.Core.Entities
+{
+    public class Transaction : BaseEntity
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public TransactionType Type { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int? CustomerId { get; set; }
+        public bool IsPosted { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual ICollection<TransactionLine> Lines { get; set; }
+    }
+}
---
+++ src/AccountingApp/AccountingApp.Core/Entities/TransactionLine.cs
@@ -0,0 +1,21 @@
+﻿using AccountingApp.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingApp.Core.Entities
+{
+    public class TransactionLine : BaseEntity
+    {
+        public int TransactionId { get; set; }
+        public int AccountId { get; set; }
+        public decimal DebitAmount { get; set; }
+        public decimal CreditAmount { get; set; }
+        public string Notes { get; set; }
+
+        public virtual Transaction Transaction { get; set; }
+        public virtual Account Account { get; set; }
+    }
+}
