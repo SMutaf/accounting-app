105304957bffa337b9991f9c72b187d4b7a8063e
Ahmet Safa Mutaf <ahmetsafamutaf@gmail.com>
2025-12-17 15:23:55 +0300

    fix: add midd using statement to entities

--- src/AccountingApp/AccountingApp.Core/Entities/Account.cs
+++ src/AccountingApp/AccountingApp.Core/Entities/Account.cs
@@ -6,6 +6,7 @@ using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
+using AccountingApp.Core.Enums;
 namespace AccountingApp.Core.Entities
 {
     public class Account : BaseEntity
--- src/AccountingApp/AccountingApp.Core/Entities/InvoiceItem.cs
+++ src/AccountingApp/AccountingApp.Core/Entities/InvoiceItem.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AccountingApp.Core.Enums;
 
 namespace AccountingApp.Core.Entities
 {
