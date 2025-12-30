441712c0dabe6d033f63d8ce160a59277a175b5b
Ahmet Safa Mutaf <ahmetsafamutaf@gmail.com>
2025-12-31 02:36:47 +0300

    Add IUnitOfWork.cs and UnitOfWork

---
+++ src/AccountingApp/AccountingApp.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingApp.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAccountRepository Accounts { get; }
+        ITransactionRepository Transactions { get; }
+        ICustomerRepository Customers { get; }
+        IInvoiceRepository Invoices { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ src/AccountingApp/AccountingApp.Data/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using AccountingApp.Core.Interfaces;
+using AccountingApp.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountingApp.Data.Context;
+
+namespace AccountingApp.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AccountingAppDbContext _context;
+
+        private IAccountRepository _accountRepository;
+        private ICustomerRepository _customerRepository;
+        private IInvoiceRepository _invoiceRepository;
+        private ITransactionRepository _transactionRepository;
+
+        public UnitOfWork(AccountingAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IAccountRepository Accounts => _accountRepository ??= new AccountRepository(_context);
+        public ICustomerRepository Customers => _customerRepository ??= new CustomerRepository(_context);
+        public IInvoiceRepository Invoices => _invoiceRepository ??= new InvoiceRepository(_context);
+        public ITransactionRepository Transactions => _transactionRepository ??= new TransactionRepository(_context);
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
