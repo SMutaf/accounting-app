bf72239e14b158c9a64c0a577024ec221682a8c5
Ahmet Safa Mutaf <ahmetsafamutaf@gmail.com>
2025-12-31 02:27:22 +0300

    add AccountBalanceDto.cs

---
+++ src/AccountingApp/AccountingApp.Core/DTOS/AccountBalanceDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingApp.Core.DTOS
+{
+    public class AccountBalanceDto
+    {
+        public int AccountId { get; set; }
+        public string AccountCode { get; set; }
+        public string AccountName { get; set; }
+        public decimal TotalDebit { get; set; }
+        public decimal TotalCredit { get; set; }
+        public decimal Balance => TotalDebit - TotalCredit;
+    }
+}
