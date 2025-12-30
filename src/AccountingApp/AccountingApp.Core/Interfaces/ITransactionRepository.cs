268481dfc938398873d93b41aa7b0d752332a798
Ahmet Safa Mutaf <ahmetsafamutaf@gmail.com>
2025-12-31 02:30:39 +0300

    add ITransactionRepository

---
+++ src/AccountingApp/AccountingApp.Core/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountingApp.Core.Entities;
+using AccountingApp.Core.DTOS;
+
+namespace AccountingApp.Core.Interfaces
+{
+    public interface ITransactionRepository : IRepository<Transaction>
+    {
+        Task<IEnumerable<Transaction>> GetByDateRangeAsync(DateTime start, DateTime end);
+        Task<IEnumerable<Transaction>> GetByCustomerAsync(int customerId);
+
+        Task<Transaction> GetByIdWithDetailsAsync(int id);
+
+        Task<IEnumerable<AccountBalanceDto>> GetAccountBalancesAsync();
+    }
+}
