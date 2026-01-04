08975bf5a2069670ddafeec4f2ba22c816448e21
Ahmet Safa Mutaf <ahmetsafamutaf@gmail.com>
2026-01-04 04:37:46 +0300

    feat (Api) : add middleware

---
+++ src/AccountingApp/AccountingApp.API/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using AccountingApp.Core.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace AccountingApp.API.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unhandled exception has occurred.");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            string message = "An internal server error occurred.";
+
+            switch (exception)
+            {
+                case NotFoundException notFoundException:
+                    statusCode = HttpStatusCode.NotFound;
+                    message = notFoundException.Message;
+                    break;
+                case ValidationException validationException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    message = validationException.Message;
+                    break;
+                case BusinessException businessException:
+                    statusCode = HttpStatusCode.BadRequest;
+                    message = businessException.Message;
+                    break;
+            }
+
+            context.Response.StatusCode = (int)statusCode;
+
+            var errorResponse = new { error = message };
+            var jsonResponse = JsonSerializer.Serialize(errorResponse);
+
+            await context.Response.WriteAsync(jsonResponse);
+        }
+    }
+}
