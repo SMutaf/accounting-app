2ced973b5fb8990bf5684fd65486b29fe761397d
Ahmet Safa Mutaf <ahmetsafamutaf@gmail.com>
2026-01-04 17:11:45 +0300

    feat: add AccountsController and register repository services

---
+++ src/AccountingApp/AccountingApp.API/Controllers/AccountsController.cs
@@ -0,0 +1,48 @@
+﻿using AccountingApp.Core.DTOS;
+using AccountingApp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountingApp.API.Controllers
+{
+
+    [ApiController]
+
+    [Route("api/[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountsController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAccounts()
+        {
+            var accounts = await _accountService.GetAllAccountsAsync();
+            return Ok(accounts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAccountById(int id)
+        {
+            var account = await _accountService.GetAccountByIdAsync(id);
+
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(account);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAccount([FromBody] AccountDto accountDto)
+        {
+            var createdAccount = await _accountService.CreateAccountAsync(accountDto);
+
+            return CreatedAtAction(nameof(GetAccountById), new { id = createdAccount.Id }, createdAccount);
+        }
+    }
+}
--- src/AccountingApp/AccountingApp.API/Program.cs
+++ src/AccountingApp/AccountingApp.API/Program.cs
@@ -1,8 +1,10 @@
+using AccountingApp.Core.Interfaces;
+using AccountingApp.Data;
 using AccountingApp.Data.Context;
-using Microsoft.EntityFrameworkCore;
 using AccountingApp.Service.Mappings;
 using AccountingApp.Services.Interfaces;
 using AccountingApp.Services.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +22,14 @@ builder.Services.AddDbContext<AccountingAppDbContext>(options =>
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 builder.Services.AddScoped<IAccountService, AccountService>();
+
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(AccountingApp.Data.Repositories.Repository<>));
+builder.Services.AddScoped<IAccountRepository, AccountingApp.Data.Repositories.AccountRepository>();
+builder.Services.AddScoped<ICustomerRepository, AccountingApp.Data.Repositories.CustomerRepository>();
+builder.Services.AddScoped<IInvoiceRepository, AccountingApp.Data.Repositories.InvoiceRepository>();
+builder.Services.AddScoped<ITransactionRepository, AccountingApp.Data.Repositories.TransactionRepository>();
 
 var app = builder.Build();
 
